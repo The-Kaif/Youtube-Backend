380790c1394de701b6fdedbbe6c453c2528bf6ae
The-Kaif <89104956+The-Kaif@users.noreply.github.com>
2024-01-11 12:53:19 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 connectDB()
-
+  .then(() => {
+    app.on("error", () => {
+      console.error("UTILS > INDEX > Error loading in express");
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("UTILS > INDEX > Error connecting database", error);
+  });
+  
